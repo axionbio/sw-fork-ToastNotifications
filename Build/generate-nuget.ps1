bfc6f6ceb258d1b3ae1febc5c403bfa8c3c7b615
Alex Gdalevich <agdalevich@axion-biosystems.com>
2020-12-01 10:03:10 -0500

    Bump version 2.5.3

--- Build/generate-nuget.ps1
+++ Build/generate-nuget.ps1
@@ -1,4 +1,4 @@
-param([string]$version="2.5.2.0")
+param([string]$version="2.5.3.0")
 
 $env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE"
 $projects = "ToastNotifications", "ToastNotifications.Messages"
